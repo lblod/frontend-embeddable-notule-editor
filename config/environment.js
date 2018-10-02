7fda219e1e4153c1c7879bc82086f2c1a9548ebe
Erika Pauwels <erika.pauwels@gmail.com>
2018-10-02 10:14:29 +0200

    Configure app for manual boot embedded in HTML page
    Since the embeddable-notule-editor will be inserted in another HTML page
    we need to prevent the router's location adapter to update the URL in
    the address bar.
    We also disable autoboot of the application on DOM ready such that the
    root element to which the Ember application will be attached can be
    configured per application instance before booting.
    To facilitate the integration in an HTML page we also package the
    configuration in the javascript package instead of a metatag and disable
    the fingerprint on the generated assets.

--- config/environment.js
+++ config/environment.js
@@ -5,7 +5,7 @@ module.exports = function(environment) {
     modulePrefix: 'frontend-embeddable-notule-editor',
     environment,
     rootURL: '/',
-    locationType: 'auto',
+    locationType: 'none',
     EmberENV: {
       FEATURES: {
         // Here you can enable experimental features on an ember canary build
@@ -20,6 +20,7 @@ module.exports = function(environment) {
     APP: {
       // Here you can pass flags/options to your application instance
       // when it is created
+      autoboot: false
     }
   };
 
