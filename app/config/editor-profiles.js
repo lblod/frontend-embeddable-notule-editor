ec5c0215e0c248b40063eed65ec5f5bcce8e2108
Felix Ruiz de Arcaute <ruiz.felixruiz@gmail.com>
2019-01-31 13:30:33 +0100

    added config

--- app/config/editor-profiles.js
+++ app/config/editor-profiles.js
@@ -1,11 +1,13 @@
 export default {
   default: [
     "rdfa-editor-citaten-plugin",
-    "rdfa-editor-standard-template-plugin"
+    "rdfa-editor-standard-template-plugin",
+    "rdfa-editor-generic-model-plugin"
   ],
   all: [
     "rdfa-editor-citaten-plugin",
-    "rdfa-editor-standard-template-plugin"
+    "rdfa-editor-standard-template-plugin",
+    "rdfa-editor-generic-model-plugin"
   ],
   none: []
 };
