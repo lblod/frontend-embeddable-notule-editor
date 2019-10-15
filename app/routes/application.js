10531c08648b9b4265e8d59acf48baf910239995
Niels Vandekeybus <progster@gmail.com>
2019-10-15 10:25:42 +0200

    commiting test setup

--- app/routes/application.js
+++ app/routes/application.js
@@ -18,7 +18,9 @@ export default Route.extend({
   model() {
     return EmberObject.create({
       title: "new document",
-      content: "<p>&nbsp;</p>",
+      content: `<div><ul class="bullet-list" data-editor-position-level="2">
+            <li><span class="mark-highlight-manual">Voeg juridische context in</span></li><li><span data-editor-highlight="true">Het besluit van de gemeenteraad van 30 april 2015 betreffende samenwerkingsovereenkomsten gemeente-OCMW</span>.</li><li><span data-editor-highlight="true">Het besluit van de OCMW-raad van 29 april 2015 betreffende 6</span><span data-editor-highlight="true"> Samenwerking OCMW </span><span data-editor-highlight="true">-gemeente: goedkeuring samenwerkingsovereenkomst</span>.</li><li data-editor-position-level="1"><span data-editor-highlight="true" data-editor-position-level="0">Het besluit van de gemeenteraad van 31 mei 2018 betreffende</span> Decreet lokaal bestuur -<span data-editor-highlight="true">Kennisname van rechtswege aanstelling algemeen directeur</span>.</li><li>Het voorgelegd ontwerp van de nieuwe beheersovereenkomst tussende gemeente en het OCMW Laarne​</li><li>Het decreet over het lokaal bestuur van 22 december 2017,<span data-editor-highlight="true"> artikel 40 en 41</span></li>
+          </ul></div>`,
       context: defaultContext
     });
   }
