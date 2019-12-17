9053a9a6d346099f79942f6b4349a928f5dd3dac
Niels Vandekeybus <progster@gmail.com>
2019-12-17 15:40:24 +0100

    update emberjs to 3.14

--- app/app.js
+++ app/app.js
@@ -3,12 +3,9 @@ import Resolver from './resolver';
 import loadInitializers from 'ember-load-initializers';
 import config from './config/environment';
 import './models/custom-inflector-rules';
-const App = Application.extend({
-  modulePrefix: config.modulePrefix,
-  podModulePrefix: config.podModulePrefix,
-  Resolver
-});
-
+export default class App extends Application {
+  modulePrefix = config.modulePrefix;
+  podModulePrefix = config.podModulePrefix;
+  Resolver = Resolver;
+}
 loadInitializers(App, config.modulePrefix);
-
-export default App;
