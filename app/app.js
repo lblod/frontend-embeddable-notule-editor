7fe2030295b9b722ea13650921d09dab6bf801c1
Sergey Andreev <sergey.andreev@redpencil.io>
2020-12-09 20:33:35 +0100

    upgraded to 3.20.5 and did some limited testing

--- app/app.js
+++ app/app.js
@@ -1,7 +1,7 @@
 import Application from '@ember/application';
 import Resolver from 'ember-resolver';
 import loadInitializers from 'ember-load-initializers';
-import config from './config/environment';
+import config from 'frontend-embeddable-notule-editor/config/environment';
 import './models/custom-inflector-rules';
 export default class App extends Application {
   modulePrefix = config.modulePrefix;
