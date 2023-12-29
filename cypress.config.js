760bde26f2ffeb48b4b7cb47942f5630f24af2e2
Rich <piemonkey@protonmail.ch>
2023-12-29 16:12:07 +0100

    Add minimal config to enable Cypress tests locally

--- cypress.config.js
+++ cypress.config.js
@@ -1,3 +1,9 @@
 const { defineConfig } = require('cypress');
 
-module.exports = defineConfig({});
+module.exports = defineConfig({
+  e2e: {
+    setupNodeEvents() {
+      // implement node event listeners here
+    },
+  },
+});
