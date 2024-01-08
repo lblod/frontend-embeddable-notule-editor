b2049284a4f6bbf79e98b390458a600da1cb8d77
Elena Poelman <elena.poelman@redpencil.io>
2024-01-08 15:41:52 +0100

    add cypress test which checks if typing inside the editor works

--- cypress/e2e/main.cy.js
+++ cypress/e2e/main.cy.js
@@ -5,4 +5,17 @@ describe('main', () => {
     cy.visit('test.html');
     cy.get('#insertBesluit').click();
   });
+
+  it('typing in the editor works as expected', () => {
+    cy.viewport(1280, 720);
+
+    cy.visit('test.html');
+
+    cy.get(`button[title='Bold']`).first().click();
+    cy.get('.say-editor__inner').type('Hello world!');
+    cy.get('.say-editor__inner').should(
+      'have.html',
+      '<p><strong>Hello world!</strong></p>'
+    );
+  });
 });
