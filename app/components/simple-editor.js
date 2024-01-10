660ac95a22079ea14a5528b9ae143fffb1976441
abeforgit <arnebertrand@gmail.com>
2024-01-10 17:28:46 +0100

    fix: make sidebar visible when needed

--- app/components/simple-editor.js
+++ app/components/simple-editor.js
@@ -118,6 +118,7 @@ export default class SimpleEditorComponent extends Component {
   @tracked initCompleted = false;
   @tracked schema;
   @tracked config;
+  @tracked uiConfig;
   @tracked nodeViews;
   @tracked activePlugins;
   @tracked citationPlugin;
