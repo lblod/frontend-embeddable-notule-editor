04fdbb3414ebc52a3b969b51dbe6565016b3be4d
Elena Poelman <elena.poelman@redpencil.io>
2024-01-11 10:47:40 +0100

    ci(cypress): reduce duplication in citation test-file

--- cypress/e2e/citation-plugin.cy.js
+++ cypress/e2e/citation-plugin.cy.js
@@ -1,40 +1,34 @@
-describe('main', () => {
+describe('citation-plugin', () => {
   beforeEach(() => {
-    cy.viewport(1280, 720);
-
-    cy.visit('test.html', {
-      // https://glebbahmutov.com/blog/cypress-tips-and-tricks/#control-navigatorlanguage
-      onBeforeLoad(win) {
-        Object.defineProperty(win.navigator, 'language', {
-          get: cy.stub().returns('en-US').as('language'),
-        });
-      },
-    });
+    cy.visit('test.html');
 
     cy.get('.say-editor-hints__insert').contains('button', 'Insert').click();
 
     cy.get('.say-editor-hints__insert')
       .contains('button', 'Insert reference')
       .click();
+
+    cy.get('.citaten--main-container')
+      .find('#searchtermexpanded')
+      .as('search-field');
+
+    cy.get('.citaten--main-container')
+      .find('.au-c-main-container__content')
+      .find('ul')
+      .as('citation-list');
   });
 
   it('the citation modal should load correctly', () => {
-    cy.get('.citaten--main-container')
-      .find('.au-c-main-container__content')
-      .find('ul')
-      .children('li')
-      .should('have.length.above', 1);
+    cy.get('@citation-list').children('li').should('have.length.above', 1);
   });
 
   it('filtering citations works', () => {
-    cy.get('.citaten--main-container').find('#searchtermexpanded').type('bos');
-    cy.get('.citaten--main-container')
-      .find('.au-c-main-container__content')
-      .find('ul')
+    cy.get('@search-field').type('bos');
+
+    cy.get('@citation-list')
       .children('li')
       .each((el) => {
         cy.wrap(el).contains('bos', { matchCase: false });
       });
   });
 });
-
