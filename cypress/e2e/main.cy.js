210a2aed645570fab857d0e336fe1ef74a7b56e6
Elena Poelman <elena.poelman@redpencil.io>
2024-01-11 10:47:29 +0100

    ci(cypress): reduce duplication in main test-file

--- cypress/e2e/main.cy.js
+++ cypress/e2e/main.cy.js
@@ -1,16 +1,12 @@
 describe('main', () => {
-  it('does basic actions', () => {
-    cy.viewport(1280, 720);
-
+  beforeEach(() => {
     cy.visit('test.html');
+  });
+  it('does basic actions', () => {
     cy.get('#insertBesluit').click();
   });
 
   it('typing in the editor works as expected', () => {
-    cy.viewport(1280, 720);
-
-    cy.visit('test.html');
-
     cy.get(`button[title='Bold']`).first().click();
     cy.get('.say-editor__inner').type('Hello world!');
     cy.get('.say-editor__inner').should(
