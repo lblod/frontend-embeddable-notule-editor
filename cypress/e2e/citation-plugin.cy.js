e09a8510f47d84c5fe077f20ab9fa79bbe32fdcf
Elena Poelman <elena.poelman@redpencil.io>
2024-01-11 10:12:17 +0100

    Reduce code cypress duplication and mock browser locale.
    Co-authored-by: Deniss Kozickis <deniss.kozickis@gmail.com>

--- cypress/e2e/citation-plugin.cy.js
+++ cypress/e2e/citation-plugin.cy.js
@@ -1,36 +1,40 @@
 describe('main', () => {
-  it('the citation modal should load correctly', () => {
+  beforeEach(() => {
     cy.viewport(1280, 720);
 
-    cy.visit('test.html');
+    cy.visit('test.html', {
+      // https://glebbahmutov.com/blog/cypress-tips-and-tricks/#control-navigatorlanguage
+      onBeforeLoad(win) {
+        Object.defineProperty(win.navigator, 'language', {
+          get: cy.stub().returns('en-US').as('language'),
+        });
+      },
+    });
+
     cy.get('.say-editor-hints__insert').contains('button', 'Insert').click();
+
     cy.get('.say-editor-hints__insert')
       .contains('button', 'Insert reference')
       .click();
+  });
+
+  it('the citation modal should load correctly', () => {
     cy.get('.citaten--main-container')
       .find('.au-c-main-container__content')
       .find('ul')
-      .as('list');
-    cy.get('@list').children('li').should('have.length.above', 1);
+      .children('li')
+      .should('have.length.above', 1);
   });
 
   it('filtering citations works', () => {
-    cy.viewport(1280, 720);
-
-    cy.visit('test.html');
-    cy.get('.say-editor-hints__insert').contains('button', 'Insert').click();
-    cy.get('.say-editor-hints__insert')
-      .contains('button', 'Insert reference')
-      .click();
     cy.get('.citaten--main-container').find('#searchtermexpanded').type('bos');
     cy.get('.citaten--main-container')
       .find('.au-c-main-container__content')
       .find('ul')
-      .as('list');
-    cy.get('@list')
       .children('li')
       .each((el) => {
         cy.wrap(el).contains('bos', { matchCase: false });
       });
   });
 });
+
