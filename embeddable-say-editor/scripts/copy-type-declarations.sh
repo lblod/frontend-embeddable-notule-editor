d1c271cc25d40d160814ef56994cb6bdeb042e4b
Rich <piemonkey@protonmail.ch>
2024-10-04 11:41:35 +0200

    Add docker-based type check

--- embeddable-say-editor/scripts/copy-type-declarations.sh
+++ embeddable-say-editor/scripts/copy-type-declarations.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 
 mkdir -p types/node_modules/@lblod/ember-rdfa-editor/core
+# Copy say-controller types from editor repo in node_modules
 cp -r node_modules/@lblod/ember-rdfa-editor/declarations/addon/core/say-controller.d.ts types/node_modules/@lblod/ember-rdfa-editor/core/
+# Add stub declaration files to avoid type errors for basic usage
 cp -r typeImports/* types/node_modules/
