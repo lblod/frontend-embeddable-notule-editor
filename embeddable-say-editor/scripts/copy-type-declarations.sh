12c50227c43a52429e274a19c618913516ecb4ee
Elena Poelman <elena.poelman@redpencil.io>
2025-02-25 12:39:57 +0100

    adjust declaration path in copy-type-declarations script

--- embeddable-say-editor/scripts/copy-type-declarations.sh
+++ embeddable-say-editor/scripts/copy-type-declarations.sh
@@ -2,6 +2,6 @@
 
 mkdir -p types/node_modules/@lblod/ember-rdfa-editor/core
 # Copy say-controller types from editor repo in node_modules
-cp -r node_modules/@lblod/ember-rdfa-editor/declarations/addon/core/say-controller.d.ts types/node_modules/@lblod/ember-rdfa-editor/core/
+cp -r node_modules/@lblod/ember-rdfa-editor/declarations/core/say-controller.d.ts types/node_modules/@lblod/ember-rdfa-editor/core/
 # Add stub declaration files to avoid type errors for basic usage
 cp -r typeImports/* types/node_modules/
