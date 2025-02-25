dec93f5bde90cb29643852eb2f195040edc74478
Elena Poelman <elena.poelman@redpencil.io>
2025-02-25 13:58:48 +0100

    add copy-editor-type-declarations.mjs script containing #root path transform

---
+++ embeddable-say-editor/scripts/copy-editor-type-declarations.mjs
@@ -0,0 +1,21 @@
+import fs from "node:fs/promises";
+
+await fs.mkdir("types/node_modules/@lblod/ember-rdfa-editor/core", {
+  recursive: true,
+});
+
+const sayControllerDeclarations = await fs.readFile(
+  "node_modules/@lblod/ember-rdfa-editor/declarations/core/say-controller.d.ts",
+  {
+    encoding: "utf-8",
+  }
+);
+
+await fs.writeFile(
+  "types/node_modules/@lblod/ember-rdfa-editor/core/say-controller.d.ts",
+  sayControllerDeclarations.replace(/#root/g, "@lblod/ember-rdfa-editor")
+);
+
+await fs.cp("typeImports/", "types/node_modules/", {
+  recursive: true,
+});
