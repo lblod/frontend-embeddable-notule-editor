198d1d99c7e21b46ef75d1899cf094fb4a91354e
Elena Poelman <elena.poelman@redpencil.io>
2025-02-27 13:19:33 +0100

    convert html-preview menu component to ts

--- embeddable-say-editor/app/components/html-preview/menu.ts
+++ embeddable-say-editor/app/components/html-preview/menu.ts
@@ -2,12 +2,17 @@ import Component from '@glimmer/component';
 import { action } from '@ember/object';
 import { tracked } from '@glimmer/tracking';
 import { generatePageForExport } from '@lblod/ember-rdfa-editor/utils/export-utils';
+import type { SayController } from '@lblod/ember-rdfa-editor';
 
-export default class HtmlPreviewMenuComponent extends Component {
+type Args = {
+  controller: SayController;
+};
+
+export default class HtmlPreviewMenuComponent extends Component<Args> {
   @tracked previewOpen = false;
 
   get exportPreview() {
-    return generatePageForExport(this.args.controller);
+    return generatePageForExport(this.args.controller, false);
   }
 
   @action
