ef2e64bfbb13dfb95e330b5055bf7f81a1e03e27
Elena Poelman <elena.poelman@redpencil.io>
2025-02-27 13:19:09 +0100

    ember: import preview types

--- embeddable-say-editor/app/app.js
+++ embeddable-say-editor/app/app.js
@@ -5,6 +5,7 @@ import config from '@lblod/embeddable-say-editor/config/environment';
 
 /**
  * @typedef {import('ember-source/types')} EmberTypes
+ * @typedef {import('ember-source/types/preview')} EmberPreviewTypes
  */
 export default class App extends Application {
   modulePrefix = config.modulePrefix;
