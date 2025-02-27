25b3993299ac155e880baded9dd2ff54bb614d74
Elena Poelman <elena.poelman@redpencil.io>
2025-02-27 12:25:22 +0100

    readd jsdoc comments

--- embeddable-say-editor/main.ts
+++ embeddable-say-editor/main.ts
@@ -50,33 +50,104 @@ type PluginName =
   | 'html-preview'
   | 'rdfa-blocks-toggle';
 
+/**
+ * An HTML element with the class `notule-editor`.
+ * These are functions available from the editor element.
+ * :warning: **`initEditor` has to be called before accessing any other methods**.
+ */
 export type EditorElement = HTMLElement & {
+  /**
+   * provides direct access to a [SayController](https://github.com/lblod/ember-rdfa-editor/blob/master/addon/core/say-controller.ts) object.
+   */
   controller: SayController;
+  /**
+   * Initialize the editor by passing an array of plugin names that should be activated and an object that contains the configuration for the editor and its plugins.
+   * See {@link file://./README.md#managing-plugins} for more info.
+   */
   initEditor: (
     arrayOfPluginNames: PluginName[],
     options: Record<string, any>
   ) => Promise<void>;
+  /**
+   * enable the banner that shows the environment and versions of plugins used.
+   */
   enableEnvironmentBanner: () => void;
+  /**
+   * disable the banner.
+   */
   disableEnvironmentBanner: () => void;
+  /**
+   * set the HTML content inside the editor, overwriting all previous content.
+   */
   setHtmlContent: (content: string) => void;
+  /**
+   * Get the HTML content of the editor.
+   * This might be different than custom content set via `setHtmlContent`, because of HTML parsing logic.
+   */
   getHtmlContent: () => string;
+  /**
+   * set the current locale of the editor.
+   * Any locale is accepted, but will fallback to `nl-BE` if it is not `nl-BE` or `en-US` (the supported languages).
+   */
   setLocale: (locale: string) => void;
+  /**
+   * returns the current locale of the editor.
+   * This will be the user's browser locale, the set local with `setLocale`, or `nl-BE`/`en-US`, the supported languages.
+   */
   getLocale: () => string;
+  /**
+   * Set the locale (language used) of the editor to Dutch.
+   */
   setLocaleToDutch: () => void;
+  /**
+   * Set the locale (language used) of the editor to English.
+   */
   setLocaleToEnglish: () => void;
 };
 
+/**
+ * The options for rendering the editor.
+ */
 export type EditorOptions = {
+  /**
+   * The HTML element to render the editor in.
+   */
   element: HTMLElement;
+  /**
+   * The title for the editor.
+   */
   title?: string;
+  /**
+   * The width of the editor.
+   */
   width: string;
+  /**
+   * The height of the editor.
+   */
   height: string;
+  /**
+   * The plugins to initialize the editor with.
+   */
   plugins?: PluginName[];
+  /**
+   * The options to initialize the editor with.
+   */
   options?: Record<string, unknown>;
+  /**
+   * Record of CSS Variables and their values to be applied to the editor.
+   */
   cssVariables?: Record<string, string>;
+  /**
+   * Whether the editor should grow to fit its content.
+   */
   growEditor?: boolean;
 };
 
+/**
+ * Renders the editor in an iframe and initializes it with the passed in plugins
+ * and options. It waits for everything to initialize and returns the fully initialized
+ * editor element, which has access to a controller and other methods (see docs)
+ */
 export async function renderEditor({
   element,
   title,
@@ -140,8 +211,8 @@ export async function renderEditor({
 
   // important here to use window.require so that webpack doesn't interfere
 
-  const App = editorFrame.contentWindow!
-    // @ts-expect-error
+  const App = editorFrame
+    .contentWindow! // @ts-expect-error
     .require('@lblod/embeddable-say-editor/app')
     .default.create({
       autoboot: false,
@@ -178,8 +249,9 @@ export async function renderEditor({
 
   if (growEditor) {
     // Set min heights to those passed
-    const sayEditorElement =
-      editorContainer.getElementsByClassName('say-editor')[0] as HTMLElement;
+    const sayEditorElement = editorContainer.getElementsByClassName(
+      'say-editor'
+    )[0] as HTMLElement;
     let topPadding, bottomPadding;
     if (sayEditorElement.computedStyleMap) {
       const stylemap = sayEditorElement.computedStyleMap();
@@ -192,8 +264,9 @@ export async function renderEditor({
     }
 
     editorPaperElement.style.minHeight = `calc(${height} - ${TOOLBAR_HEIGHT} - ${topPadding} - ${bottomPadding})`;
-    const sayContentElement =
-      editorPaperElement.getElementsByClassName('say-content')[0] as HTMLElement;
+    const sayContentElement = editorPaperElement.getElementsByClassName(
+      'say-content'
+    )[0] as HTMLElement;
     sayContentElement.style.minHeight = `calc(${height} - ${TOOLBAR_HEIGHT} - ${topPadding} - ${bottomPadding})`;
 
     // Resize to fit content
