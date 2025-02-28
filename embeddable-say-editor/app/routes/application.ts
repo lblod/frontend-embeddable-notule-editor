d0cef7629c9be22e7ed3e54206e924d8ac2c16e3
Elena Poelman <elena.poelman@redpencil.io>
2025-02-28 17:52:05 +0100

    convert application route to ts

---
+++ embeddable-say-editor/app/routes/application.ts
@@ -0,0 +1,14 @@
+import Route from '@ember/routing/route';
+import { inject as service } from '@ember/service';
+import type Transition from '@ember/routing/transition';
+import IntlService from 'ember-intl/services/intl';
+
+export default class ApplicationRoute extends Route {
+  @service declare intl: IntlService;
+
+  beforeModel(transition: Transition) {
+    const userLocale = navigator.language ?? navigator.languages[0];
+    this.intl.setLocale([userLocale, 'nl-BE']);
+    return super.beforeModel(transition);
+  }
+}
