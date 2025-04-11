40453bad935d5012a5fc4b79da5b60dd58e86c05
abeforgit <arnebertrand@gmail.com>
2025-04-11 23:11:24 +0200

    cleanup: delete old app

--- embeddable-say-editor.bak/shared-types/index.ts
+++
@@ -1,3 +0,0 @@
-/**
- * @file The `shared-types` folder contains types which may be used by both the embeddable-app, and the package files
- */
--- embeddable-say-editor.bak/type-imports/@lblod/ember-rdfa-editor/core/say-view.d.ts
+++
@@ -1,5 +0,0 @@
-export default class SayView {}
-export interface SetHtmlOptions {
-  shouldFocus?: boolean;
-  doNotClean?: boolean;
-}
