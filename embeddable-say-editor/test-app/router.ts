a22dd98be265483297ca533f3a91a99f65bdbd9d
Elena Poelman <elena.poelman@redpencil.io>
2025-04-14 13:24:08 +0200

    test-app: add seperate vite config file to build and run test-app

--- embeddable-say-editor/test-app/router.ts
+++ embeddable-say-editor/test-app/router.ts
@@ -11,12 +11,11 @@ const navbar = document.createElement('template');
 
 const html = String.raw;
 navbar.innerHTML = html`<div class="navbar">
-  <a href="/test-app/">index</a>
-  <a href="/test-app/multiple-editors/">Multiple Editors</a>
-  <a href="/test-app/multiple-growing-editors/">Multiple growing editors</a>
-  <a href="/test-app/plugins/">Plugins</a>
-  <a href="/test-app/plugins-with-rdfa-editor/">Plugins with RDFa editor</a>
-  <a href="/test-app/cipal-plugins/">cipal-plugins</a>
+  <a href="/">index</a>
+  <a href="/multiple-editors/">Multiple Editors</a>
+  <a href="/multiple-growing-editors/">Multiple growing editors</a>
+  <a href="/plugins/">Plugins</a>
+  <a href="/cipal-plugins/">cipal-plugins</a>
 </div>`;
 
 export const router = navbar.content;
