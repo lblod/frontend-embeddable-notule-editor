05b406a74f0a39a498793886649a666b8a535fc2
abeforgit <arnebertrand@gmail.com>
2026-06-19 12:03:20 +0200

    feat: add example transformation that actually does something

--- embeddable-say-editor/test-app/custom-headless-editing/headless-editing.ts
+++ embeddable-say-editor/test-app/custom-headless-editing/headless-editing.ts
@@ -44,17 +44,22 @@ if (editorBeforeElement && editorAfterElement) {
 }
 
 document.getElementById('replace')?.addEventListener('click', () => {
-  const besluit = (
+  const sampledoc = (
     document.getElementById('besluit') as unknown as HTMLInputElement
   ).value;
 
   const myCustomProcessor = (state: EditorState) => {
     const tr = state.tr;
+    tr.replaceRangeWith(
+      0,
+      tr.doc.nodeSize - 2,
+      state.schema.text('replaced with something else!'),
+    );
     return { initialState: state, transaction: tr, result: true };
   };
 
   const html = processDocumentHeadlessly(
-    besluit,
+    sampledoc,
     (state) => transactionCombinator<boolean>(state)([myCustomProcessor]),
     pluginDemoConfig,
   );
@@ -64,5 +69,5 @@ document.getElementById('replace')?.addEventListener('click', () => {
 
 document.getElementById('sample-document')?.addEventListener('click', () => {
   (document.getElementById('besluit') as unknown as HTMLInputElement).value =
-    '<div data-content-container="true"><p class="say-paragraph"><span data-locked-placeholder="true" data-locked-placeholder-type="inline" data-label="Inline" data-key="inline"></span><span data-locked-placeholder="true" data-locked-placeholder-type="inline" data-label="Second Inline" data-key="inline"></span></p><div data-locked-placeholder="true" data-locked-placeholder-type="block" data-label="Block" data-key="block"></div></div>';
+    'hello world';
 });
